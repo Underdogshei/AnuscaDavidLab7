90cac4a49a225dd34cd1637305e1b182062d5a89
Anusca David <davidanusca112@gmail.com>
2024-11-26 13:32:14 +0200

    Add project files.

---
+++ ListPage.xaml.cs
@@ -0,0 +1,12 @@
+using AnuscaDavidLab7.Models;
+namespace AnuscaDavidLab7;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e) { var slist = (ShopList)BindingContext; slist.Date = DateTime.UtcNow; await App.Database.SaveShopListAsync(slist); await Navigation.PopAsync(); }
+    async void OnDeleteButtonClicked(object sender, EventArgs e) { var slist = (ShopList)BindingContext; await App.Database.DeleteShopListAsync(slist); await Navigation.PopAsync(); }
+}
